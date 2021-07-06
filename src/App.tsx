f9abd35b80493587451c6eb17a56eb79bcb62234
TBlackford <c4L4M1TY>
2021-07-06 22:30:57 +1200

    Changed to reach router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
+import { Router, RouteComponentProps, Link } from "@reach/router"
+
 import './App.css';
 
-const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
-    </div>
-);
+let Home = (props: RouteComponentProps) => <div>Home</div>
+let Dash = (props: RouteComponentProps) => <div>Dash</div>
+
+
+const App: React.FunctionComponent = (props) => {
+    return (
+        <div className="main-content">
+            <Router>
+                <Home path="/" />
+                <Dash path="dashboard" />
+            </Router>
+        </div>
+    );
+};
+
 
 export default App;
