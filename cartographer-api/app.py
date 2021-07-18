0009e0a097b958e92a29f91b20b1e37d897f538d
TBlackford <c4L4M1TY>
2021-07-18 21:01:36 +1200

    removed route

--- cartographer-api/app.py
+++ cartographer-api/app.py
@@ -79,11 +79,6 @@ def hello_from_root(year):
     return make_response(jsonify(res), 404)
 
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
-
-
 @app.errorhandler(404)
 def resource_not_found(e):
     return make_response(jsonify(error='Not found!'), 404)
